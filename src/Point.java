8edd97dfcf0e78f70efcf37264b08f0241e31e6a
Daniel Kończyk <daniel@konczyk.net>
2016-06-09 06:08:39 +0200

    Add more Point comparators and squared distance

--- src/Point.java
+++ src/Point.java
@@ -1,8 +1,14 @@
+import java.util.Comparator;
+import java.util.Objects;
+
 /**
  * A point in the plane
  */
 
 public final class Point implements Comparable<Point> {
+
+    public static final Comparator<Point> X_ORDER = new XOrder();
+    public static final Comparator<Point> Y_ORDER = new YOrder();
 
     private final double x;
     private final double y;
@@ -21,10 +27,14 @@ public final class Point implements Comparable<Point> {
     }
 
     public double distanceTo(Point other) {
+        return Math.sqrt(distanceSquaredTo(other));
+    }
+
+    public double distanceSquaredTo(Point other) {
         double dx = other.x - x;
         double dy = other.y - y;
 
-        return Math.sqrt(dx*dx + dy*dy);
+        return dx*dx + dy*dy;
     }
 
     @Override
@@ -37,4 +47,35 @@ public final class Point implements Comparable<Point> {
         }
     }
 
+    private static class XOrder implements Comparator<Point> {
+        @Override
+        public int compare(Point p, Point q) {
+            return Double.compare(p.x, q.x);
+        }
+    }
+
+    private static class YOrder implements Comparator<Point> {
+        @Override
+        public int compare(Point p, Point q) {
+            return Double.compare(p.y, q.y);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Point point = (Point) o;
+        return Double.compare(point.x, x) == 0
+            && Double.compare(point.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
--- test/PointTest.java
+++ test/PointTest.java
@@ -27,11 +27,11 @@ public class PointTest {
 
     @Test
     @Parameters({
-        " 1,   2,  3,   1,  1",
-        " 1,   2,  3,   3, -1",
-        "-3,   2, -4,   2,  1",
-        "-3,   2, -1,   2, -1",
-        " 0.0, 3, -0.0, 3,  0"})
+        " 1.0, 2.0,  3.0, 1.0,  1",
+        " 1.0, 2.0,  3.0, 3.0, -1",
+        "-3.0, 2.0, -4.0, 2.0,  1",
+        "-3.0, 2.0, -1.0, 2.0, -1",
+        " 0.0, 3.0, -0.0, 3.0,  0"})
     public void compareTo(double x1, double y1,
                           double x2, double y2, int result) {
         Point p1 = new Point(x1, y1);
@@ -42,14 +42,52 @@ public class PointTest {
 
     @Test
     @Parameters({
-        "1, 2, 3, 1, 2.236",
-        "5, -2, -8, -2.3, 13.003"})
+        "1.0,  2.0,  3.0,  1.0,  2.236",
+        "5.0, -2.0, -8.0, -2.3, 13.003"})
     public void distanceTo(double x1, double y1,
                            double x2, double y2, double result) {
         Point p1 = new Point(x1, y1);
         Point p2 = new Point(x2, y2);
 
         assertThat(p1.distanceTo(p2), is(closeTo(result, DELTA)));
+    }
+
+    @Test
+    @Parameters({
+        "1.0,  2.0,  3.0,  1.0,   5.0",
+        "5.0, -2.0, -8.0, -2.3, 169.09"})
+    public void distanceSquaredTo(double x1, double y1,
+                                  double x2, double y2, double result) {
+        Point p1 = new Point(x1, y1);
+        Point p2 = new Point(x2, y2);
+
+        assertThat(p1.distanceSquaredTo(p2), is(closeTo(result, DELTA)));
+    }
+
+    @Test
+    @Parameters({
+        " 1.0, 2.0,  3.0, 1.0, -1",
+        "-3.0, 2.0, -4.0, 2.0,  1",
+        " 0.0, 3.0, -0.0, 3.0,  0"})
+    public void compareByX(double x1, double y1,
+                           double x2, double y2, int result) {
+        Point p1 = new Point(x1, y1);
+        Point p2 = new Point(x2, y2);
+
+        assertThat(Point.X_ORDER.compare(p1, p2), is(result));
+    }
+
+    @Test
+    @Parameters({
+        " 1.0, 2.0,  3.0,  1.0,  1",
+        "-3.0, 1.0, -4.0,  2.0, -1",
+        " 3.0, 0.0,  3.0, -0.0,  0"})
+    public void compareByY(double x1, double y1,
+                           double x2, double y2, int result) {
+        Point p1 = new Point(x1, y1);
+        Point p2 = new Point(x2, y2);
+
+        assertThat(Point.Y_ORDER.compare(p1, p2), is(result));
     }
 
 }
